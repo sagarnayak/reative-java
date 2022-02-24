d02c6c3519b1d79091fb051a3a3c0692d06849ec
sagar <snkumar.nayak@gmail.com>
2022-02-24 17:57:38 +0530

    initial commit

---
+++ src/main/kotlin/com/sagar/reativejava/ReativeJavaApplication.kt
@@ -0,0 +1,11 @@
+package com.sagar.reativejava
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReativeJavaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReativeJavaApplication>(*args)
+}
---
+++ src/test/kotlin/com/sagar/reativejava/ReativeJavaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sagar.reativejava
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReativeJavaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
