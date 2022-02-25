3990db55ca9cb865273c219e79f0d52d19b0a852
sagar <snkumar.nayak@gmail.com>
2022-02-25 13:07:30 +0530

    updated

--- src/main/kotlin/com/sagar/reativejava/controllers/StreamFirst.kt
+++ src/main/kotlin/com/sagar/reativejava/controllers/StreamFirst.kt
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
@@ -13,5 +14,10 @@ class StreamFirst {
     @GetMapping
     fun getData(): Mono<String> {
         return Mono.just(Faker.instance().animal().name())
+    }
+
+    @GetMapping("/flux")
+    fun getFluxData(): Flux<Int> {
+        return Flux.range(2, 2)
     }
 }
